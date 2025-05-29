a3c9fd4b7aa8dc2168f08eabd823cb6415820f7d
kaustav234 <kaustavgupta2311@gmail.com>
2025-05-29 14:33:07 +0530

    Global and local scopes

--- 03_scopes.js
+++ 03_scopes.js
@@ -1,10 +1,21 @@
+var c = 300; // global scope which is available outside the loop
 
+let a = 100;
 
 if(true) {
     let a = 10
     const b = 20
     var c = 30
+    console.log("INNER:", a);
+} // block scope
+
+
+console.log("OUTER:",a); 
+// console.log(b);
+console.log(c);
+
+for(let i = 0; i < array.length; i++) {
+    const element = array[i];
 }
 
-
-console.log(a, b, c); 
+ 
---
+++ 04_scopes.js
@@ -0,0 +1,43 @@
+// function one(){
+//     const username = "kaustav"
+
+//     function two() {
+//         const website = "youtube"
+//         console.log(username);
+//     }
+//     //console.log(website);
+
+//     two();
+// }
+// //one();
+
+
+// if(true){
+//     const username = "kaustav";
+//     if(username === "kaustav"){
+//         const website = " youtube";
+//         console.log(username + website);
+//     }
+//     console.log(website);
+    
+// }
+// console.log(username);
+
+
+
+
+
+// -------------------------------------- INTERESTING ------------------------------------------
+
+console.log(addOne(2));
+function addOne(num) {
+    return num + 1;
+}
+
+// --------------------
+
+console.log(addTwo(5));
+const addTwo = function(num){
+    return num + 2;
+}
+
