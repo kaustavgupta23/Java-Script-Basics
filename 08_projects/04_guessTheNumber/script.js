5cc11611a3d6a2b6fcb3a283b30b53a3304b0031
kaustavgupta <kaustavgupta2311@gmail.com>
2025-06-13 23:49:30 +0530

    project_number-guess added

--- 08_projects/04_guessTheNumber/script.js
+++ 08_projects/04_guessTheNumber/script.js
@@ -1,4 +1,6 @@
-const random = parseInt(Math.random() * 100 + 1);
+let randomNumber = parseInt(Math.random() * 100 + 1);
+console.log(randomNumber);
+
 
 const submit = document.querySelector('#subt');
 const userInput = document.querySelector('#guessField');
@@ -9,19 +11,109 @@ const startOver = document.querySelector('.resultParas');
 
 const p = document.createElement('p');
 
-let prevGuess = []; // array isliye liya taki jo bhi user submit krega values ko store krlega or user ko dhikayega taki same values user guess na kare
+let prevGuess = [];
+//  already guessed value ko user firse na let
 let numGuess = 1;
+// kitne number of guess vo le skta hai, 1 se 10 tak
+// 10 tak phauch ke guess btn ko disable krdenge
 
 let playGame = true;
 
-function valiadateGuess(guess){
-    //value <1 or >100 toh nai hai
+if(playGame){
+    submit.addEventListener('click', function(e){
+        e.preventDefault();
+        const guess = parseInt(userInput.value);
+        console.log(guess);
+        validateGuess(guess);
+    })
+}
+
+function validateGuess(guess){
+    // kya value sahi ja rhi hai??
+    // kahi <1 or > 100 toh ni ja rhi
+    if(isNaN(guess)){
+        alert("Please enter a valid number")
+    } else if(guess < 1){
+        alert("Please enter a number more than 1")
+    } else if(guess > 100){
+        alert("Please enter a number less than 100")
+    } else{
+        prevGuess.push(guess);
+        if(numGuess > 10){
+            cleanupGuess(guess);
+            displayMessage(`Game Over, random number was ${randomNumber}`);
+            endGame();
+        } else {
+            cleanupGuess(guess);
+            checkGuess(guess);
+        }
+    }
+
 }
 
 function checkGuess(guess){
-    // 
+    // need to print ki value sahi hai ya galat
+    // value sahi h ya galat
+    // low or high values toh nahi hai?
+
+    if(guess === randomNumber){
+        displayMessage(`Right guess`);
+        endGame();
+    } else if(guess < randomNumber){
+        displayMessage(`Number is too low`);
+    } else if(guess > randomNumber){
+        displayMessage(`Number is too high`);
+    }
 }
 
-function displayMessage(){
+function cleanupGuess(guess){
     // 
+    userInput.value = ''; // value clean krdo
+    guessSlot.innerHTML += `${guess}, `
+    numGuess++;
+    remaining.innerHTML = `${11 - numGuess}`;
 }
+
+function displayMessage(message){
+    // DOM ke sath interect krega
+    // user se message leke print krega
+    lowOrHi.innerHTML = `<h2>${message}</h2>`
+
+
+}
+
+function endGame(){
+    userInput.value = '' // value clean;
+    userInput.setAttribute('disabled', '')
+    p.classList.add('button')
+    p.innerHTML =  `<h2 id="newGame">Start new Game</h2>`
+    startOver.appendChild(p);
+    playGame = false;
+    newGame();
+
+}
+
+function newGame(){
+    // 
+    const newGameButton = document.querySelector('#newGame');
+    newGameButton.addEventListener('click', function(e){
+        randomNumber = parseInt(Math.random() * 100 + 1);
+        prevGuess = [];
+        numGuess = 1;
+        guessSlot.innerHTML = '';
+        remaining.innerHTML = `${11 - numGuess}`;
+        userInput.removeAttribute('disabled')
+        startOver.removeChild(p);
+
+        playGame = true;
+    })
+}
+
+
+
+
+
+
+
+
+
